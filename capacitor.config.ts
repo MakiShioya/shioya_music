988eb9a711cc338e9ecb814d3c25be192bf161b3
MakiShioya <makimakisioya@gmail.com>
2026-05-13 14:01:53 +0900

    データ同期

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shioyasoft.music',
+  appName: 'Shioya Music',
+  webDir: 'public'
+};
+
+export default config;
